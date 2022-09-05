aec99da745ae158548a4fc93ef989b4845d9f36e
Hahn, Eduardo <eduardo.hahn@adesso.ch>
2022-09-05 17:16:16 +0200

    Create solver class and more incrementors

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "utils/csv.hpp"
-#include "solver/creators/operation-producer.hpp"
 
-int main(int argc, char **argv) {
+#include "solver/solver.hpp"
+
+vector<Variable> readVariables(int argc, char *const *argv) {
     if (argc < 4) {
         throw invalid_argument("Please provide 2 paths for input and result csv files followed by the variable names.");
     }
@@ -13,17 +14,16 @@ int main(int argc, char **argv) {
     if(input.size() != results.size()) {
         throw length_error("Both files need to have the same number of lines.");
     }
-    
+
     vector<Variable> variables;
     for(int i = 3; i < argc; ++i) {
         variables.emplace_back(argv[i]);
     }
-    
-    OperationProducer operationProducer;
-    const shared_ptr<Node> &operation = operationProducer.produce(variables);
-    Formula formula(operation, variables);
+    return variables;
+}
 
-    // TODO: Alter formula with changers and rate the results. Store them in a list and prefer the best changers.
-
+int main(int argc, char **argv) {
+    Solver solver(readVariables(argc, argv));
+    solver.start();
     return 0;
 }
--- src/solver/solver.hpp
+++ src/solver/solver.hpp
@@ -2,7 +2,10 @@
 #define GENERIC_SOLVER_SOLVER_HPP
 
 #include "solver-base.hpp"
+#include "creators/operation-producer.hpp"
 #include <cmath>
+#include <thread>
+#include <utility>
 
 class Solution {
 public:
@@ -10,11 +13,11 @@ public:
 
     bool operator<(Solution &other) const { return (this->rate < other.rate); }
 
-    Formula &getForumla() const { return formula; }
+    [[nodiscard]] Formula &getForumla() const { return formula; }
 
-    ChangerType getLastChanger() const { return lastChanger; }
+    [[nodiscard]] ChangerType getLastChanger() const { return lastChanger; }
 
-    number getRate() const { return rate; }
+    [[nodiscard]] number getRate() const { return rate; }
 
 private:
     Formula &formula;
@@ -37,10 +40,44 @@ public:
         size_t records = expected.size();
 
         for (auto i = 0; i < records; ++i) {
-            result += expected[i][0] / formula.evaluate(input[i]) / records;
+            result += expected[i][0] / formula.evaluate(input[i][0]) / records;
         }
         return result == 0 ? 0 : 1 - abs(1 - result);
     }
+};
+
+class Solver {
+public:
+    template<typename R>
+    explicit Solver(vector<Variable> variables, R &randomNumber) : variables(std::move(variables)),
+                                                                   cores(thread::hardware_concurrency()),
+                                                                   currentState(READY), operationProducer(
+                    make_shared<OperationProducer>(OperationProducer(randomNumber))) {}
+
+    Solver(vector<Variable> variables) : variables(std::move(variables)), cores(thread::hardware_concurrency()),
+                                         currentState(READY), operationProducer(make_shared<OperationProducer>()) {}
+
+    void start() {
+        const shared_ptr<Node> &operation = operationProducer->produce(variables);
+        Formula formula(operation, variables);
+
+        // TODO: Alter formula with changers and rate the results. Store them in a list and prefer the best changers.
+
+    }
+
+    [[nodiscard]] SolverState getState() const {
+        return currentState;
+    }
+
+    void updateState(SolverState state) {
+        this->currentState = state;
+    }
+
+private:
+    shared_ptr<OperationProducer> operationProducer;
+    SolverState currentState;
+    vector<Variable> variables;
+    unsigned int cores;
 };
 
 set<Solution> solutions;
