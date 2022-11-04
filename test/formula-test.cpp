37a2e08fa6c7ef5e43de1f76b150993e4daa89eb
Hahn, Eduardo <eduardo.hahn@adesso.ch>
2022-11-04 15:19:29 +0100

    First working state (unstable but delivering results)

--- test/formula-test.cpp
+++ test/formula-test.cpp
@@ -1,6 +1,15 @@
 #include "test.hpp"
+#include "../src/solver/creators/operation-producer.hpp"
 
-TEST_CASE("Formula evaluation") {
+TEST_CASE("Formula evaluation square") {
+    Variable d("d");
+    Square square(d);
+    Formula formula(square, d);
+    CHECK(formula.toString() == "(d)²");
+    CHECK(format(formula.evaluate(5)) == "25");
+}
+
+TEST_CASE("Formula evaluation multiplication") {
     Variable r("r", 1);
     Multiplication circleArea(Square(r), PI);
     Formula formula(circleArea, r);
@@ -8,4 +17,14 @@ TEST_CASE("Formula evaluation") {
     CHECK((formula.evaluate(1) == PI.calculate()));
     CHECK(format(formula.evaluate(4)) == "50.265482");
     CHECK(format(formula.evaluate(200)) == "125663.71");
+}
+
+TEST_CASE("Formula evaluation operation producer") {
+    OperationProducer operationProducer(testRandomNumber);
+    vector<Variable> vec;
+    vec.emplace_back("x");
+    auto node = operationProducer.produce(vec);
+    Formula formula(node, vec);
+    CHECK(formula.toString() == "(x)³");
+    CHECK(format(formula.evaluate(2)) == "8");
 }
